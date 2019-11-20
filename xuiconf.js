3f6eb5892353709ffa3b7612e740028c67053af8
fred1786 <57971159+fred1786@users.noreply.github.com>
2019-11-20 09:08:17 +0800

    Updated by CrossUI No-Code App Builder

--- xuiconf.js
+++ xuiconf.js
@@ -1,6 +1,23 @@
 
+
 // [[Page Appearance
 xui.ini.$PageAppearance = {
     "theme":"default"
 };
 // ]]Page Appearance
+
+
+// [[Font Icons CDN
+
+// ]]Font Icons CDN
+
+
+// [[Develop Env Setting
+xui.ini.$DevEnv = {
+    "designViewConf":{
+        "touchDevice":true,
+        "width":800,
+        "height":600
+    }
+};
+// ]]Develop Env Setting
